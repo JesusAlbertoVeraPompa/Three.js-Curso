341ea988d0815e991cec397c41bf9988df148314
Jesus Pompa <jesusverapompa@gmail.com>
2026-02-21 09:34:20 -0500

    1.0.3 - Prueba con Vite en Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/Three.js-Curso/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        // Tu página principal
+        main: resolve(__dirname, 'index.html'),
+        // Debes agregar cada página secundaria aquí
+        textures: resolve(__dirname, 'src/pages/10.0-Textures.html'),
+        // Agrega las demás páginas que tengas siguiendo el mismo formato
+      }
+    }
+  }
 })
